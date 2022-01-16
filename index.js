f41b198122edc0d1c2b91b5f3c36788f917ecf4b
ppojun <120106@naver.com>
2022-01-16 15:24:24 +0900

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = "hong", age = 24, gender = "male";
+var sayHi = function (name, age, gender) {
+    console.log("Hello ".concat(name, ", you are ").concat(age, ", you are a ").concat(gender));
+};
+sayHi(name, age, gender);
+//# sourceMappingURL=index.js.map
