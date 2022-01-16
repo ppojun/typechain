c1261bc4709c7296466111628f0d8bfc8003b64e
ppojun <120106@naver.com>
2022-01-16 17:03:52 +0900

    ts interface

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var name = "hong", age = 24, gender = "male";
-var sayHi = function (name, age, gender) {
-    console.log("Hello ".concat(name, ", you are ").concat(age, ", you are a ").concat(gender));
-};
-sayHi(name, age, gender);
-//# sourceMappingURL=index.js.map
--- index.ts
+++
@@ -1,7 +0,0 @@
-const sayHi = (name: string, age: number, gender: string)=>{
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-};
-
-sayHi("hh",444,"male");
-
-export {};
